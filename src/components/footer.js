d202c6daa11c3801bd1bf7ff2e0854df81e6ef63
metafarmers-eu <metafarmers@proton.me>
2024-10-18 03:30:33 +0100

    navigation

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,6 @@
 // src/components/footer.js
 import React from 'react';
 
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-
 const Footer = () => {
   return (
   <div className="col-lg-8 mx-auto p-4 py-md-5">
