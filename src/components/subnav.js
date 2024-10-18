0b0dd40586c651cf9ff0e9fa8932b3b2044bf059
metafarmers-eu <metafarmers@proton.me>
2024-10-18 04:20:23 +0100

    navigation

--- src/components/subnav.js
+++ src/components/subnav.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Subnav = () => {
   return (
-    <div className="col-lg-8 mx-auto p-4 py-md-5">
+   
       <ul className="nav nav-underline">
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/home">Active</Link>
@@ -19,7 +19,7 @@ const Subnav = () => {
           <span className="nav-link disabled" aria-disabled="true">Disabled</span>
         </li>
       </ul>
-    </div>
+
   );
 };
 
