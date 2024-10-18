b56c4c0ee3e60f2ae9159f3bbe9886424e5852d1
metafarmers-eu <metafarmers@proton.me>
2024-10-18 02:53:05 +0100

    navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,19 +5,28 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Navigation = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/">Meta Farmers</Navbar.Brand> {/* Updated Link */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>  {/* Link to Home */}
-            <Nav.Link as={Link} to="/features">Features</Nav.Link>  {/* Link to Features */}
-            <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>  {/* Link to Pricing */}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header className="d-flex align-items-center pb-3 mb-5 border-bottom">
+      <Link to="/" className="d-flex align-items-center text-body-emphasis text-decoration-none">
+        <svg className="bi me-2" width="40" height="32">
+          <use xlinkHref="#bootstrap" />
+        </svg>
+        <span className="fs-4">Meta Farmers</span>
+      </Link>
+
+      <nav className="ms-auto">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/features">Features</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/pricing">Pricing</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
