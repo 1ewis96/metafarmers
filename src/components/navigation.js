01c279e6da899ac12c25557deb8016832e479a07
metafarmers-eu <metafarmers@proton.me>
2024-10-18 18:02:54 +0100

    navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,28 +1,29 @@
-// src/components/navigation.js
 import React from 'react';
-
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Navigation = () => {
   return (
-  <div className="col-lg-8 mx-auto p-4 py-md-5">
-    <header className="d-flex align-items-center pb-3 mb-5 border-bottom">
-      <Link to="/" className="d-flex align-items-center text-body-emphasis text-decoration-none">
-        <svg className="bi me-2" width="40" height="32">
-          <use xlinkHref="#bootstrap" />
-        </svg>
-        <span className="fs-4">Meta Farmers</span>
-      </Link>
+    <div className="container">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div className="col-md-3 mb-2 mb-md-0">
+          <Link to="/" className="d-inline-flex link-body-emphasis text-decoration-none">
+            <svg className="bi" width="40" height="32" role="img" aria-label="Bootstrap">
+              <use xlinkHref="#bootstrap" />
+            </svg>
+            <span className="fs-4 ms-2">Meta Farmers</span>
+          </Link>
+        </div>
 
-      <nav className="ms-auto">
-        <ul className="nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/registration">Join Now</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-	</div>
+        <div className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          Some text.
+        </div>
+
+        <div className="col-md-3 text-end">
+
+          <Link className="btn btn-primary" to="/registration">Join Now</Link>
+        </div>
+      </header>
+    </div>
   );
 };
 
