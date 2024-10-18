478765ff7a34a00852fccd295d185329db96238f
metafarmers-eu <metafarmers@proton.me>
2024-10-18 03:37:54 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React from 'react';
-import Navigation from './components/navigation';
-import Footer from './components/footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 import Home from './pages/Home';          // Import Home component
 import Features from './pages/Features';  // Import Features component
 import Pricing from './pages/Pricing';    // Import Pricing component
@@ -13,14 +13,17 @@ function App() {
       <div className="App">
         <Navigation />
         <Routes>
-          <Route path="/" element={<Home />} />        {/* Home route */}
+          {/* Redirect from '/' to '/home' */}
+          <Route path="/" element={<Navigate to="/home" />} />
+
+          {/* Explicit routes */}
+          <Route path="/home" element={<Home />} />        {/* Home route */}
           <Route path="/features" element={<Features />} /> {/* Features route */}
           <Route path="/pricing" element={<Pricing />} />  {/* Pricing route */}
         </Routes>
-		<Footer />
+        <Footer />
       </div>
     </Router>
-
   );
 }
 
