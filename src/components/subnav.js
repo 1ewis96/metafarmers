ac4dd96e5e015942561d6990d6ea4fe20bfd2170
metafarmers-eu <metafarmers@proton.me>
2024-10-18 04:15:44 +0100

    navigation

--- src/components/subnav.js
+++ src/components/subnav.js
@@ -1,26 +1,25 @@
 // src/components/subnav.js
 import React from 'react';
-
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Subnav = () => {
   return (
-  <div className="col-lg-8 mx-auto p-4 py-md-5">
-	<ul className="nav nav-underline">
-	  <li className="nav-item">
-		<a className="nav-link active" aria-current="page" href="#">Active</a>
-	  </li>
-	  <li className="nav-item">
-		<a className="nav-link" href="#">Link</a>
-	  </li>
-	  <li className="nav-item">
-		<a className="nav-link" href="#">Link</a>
-	  </li>
-	  <li className="nav-item">
-		<a className="nav-link disabled" aria-disabled="true">Disabled</a>
-	  </li>
-	</ul>
-	</div>
+    <div className="col-lg-8 mx-auto p-4 py-md-5">
+      <ul className="nav nav-underline">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/home">Active</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/features">Link</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/pricing">Link</Link>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
+// src/pages/Home.js
 import React from 'react';
-import Subnav from './components/subnav';
+import Subnav from '../components/subnav'; // Ensure correct import path
 
 const Home = () => {
   return (
     <div className="col-lg-8 mx-auto p-4 py-md-5">
       <h1>Home Page</h1>
       <p>Welcome to Meta Farmers!</p>
+      <Subnav /> {/* Ensure Subnav is rendered within the main content */}
     </div>
-	<Subnav />
   );
 };
 
