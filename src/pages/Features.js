d7c8419cf2ef35558aa5e59e1ba63909942dbbaf
metafarmers-eu <metafarmers@proton.me>
2024-10-18 04:40:13 +0100

    navigation

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Subnav from '../components/subnav'; // Ensure correct import path
 const Features = () => {
   return (
+  <>
+  <Subnav />
     <div className="col-lg-8 mx-auto p-4 py-md-5">
       <h1>Features Page</h1>
       <p>Discover the amazing features of Meta Farmers.</p>
     </div>
+	</>
   );
 };
 
--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Subnav from '../components/subnav'; // Ensure correct import path
 const Pricing = () => {
   return (
+	<>
+	<Subnav />
     <div className="col-lg-8 mx-auto p-4 py-md-5">
       <h1>Pricing Page</h1>
       <p>Check out our pricing plans!</p>
     </div>
+	</>
   );
 };
 
