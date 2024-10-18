1015d52961a052ba823e1e71d0c8d94515d1d05a
metafarmers-eu <metafarmers@proton.me>
2024-10-18 04:30:35 +0100

    navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,35 @@ import Subnav from '../components/subnav'; // Ensure correct import path
 
 const Home = () => {
   return (
+  
+    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="https://placehold.co/1500x400/EEE/31343C" className="d-block w-100" alt="First slide" />
+        </div>
+        <div className="carousel-item">
+          <img src="https://placehold.co/1500x400/EEE/31343C" className="d-block w-100" alt="Second slide" />
+        </div>
+        <div className="carousel-item">
+          <img src="https://placehold.co/1500x400/EEE/31343C" className="d-block w-100" alt="Third slide" />
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+	
+        <Subnav /> {/* Ensure Subnav is rendered within the main content */}
+		
     <div className="col-lg-8 mx-auto p-4 py-md-5">
       <h1>Home Page</h1>
       <p>Welcome to Meta Farmers!</p>
-      <Subnav /> {/* Ensure Subnav is rendered within the main content */}
+
     </div>
   );
 };
