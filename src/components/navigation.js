8f98676ff42d4c5634dcb01efa8cd381ca250275
metafarmers-eu <metafarmers@proton.me>
2024-10-18 18:08:58 +0100

    navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Navigation = () => {
   return (
-    <div className="container bg-dark">
+   <>
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-dark">
-        <div className="col-md-3 mb-2 mb-md-0">
+         <div className="container bg-dark">
+		 <div className="col-md-3 mb-2 mb-md-0">
           <Link to="/" className="d-inline-flex link-body-emphasis text-decoration-none">
             <svg className="bi" width="40" height="32" role="img" aria-label="Bootstrap">
               <use xlinkHref="#bootstrap" />
@@ -22,8 +23,9 @@ const Navigation = () => {
 
           <Link className="btn btn-primary" to="/registration">Join Now</Link>
         </div>
+		</div>
       </header>
-    </div>
+</>
   );
 };
 
