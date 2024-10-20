ac243af7a790177bb94abdee1a52c9029e89bd76
metafarmers-eu <metafarmers@proton.me>
2024-10-20 02:23:29 +0100

    changes?

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Subnav from '../components/subnav'; // Ensure correct import path
+const Login = () => {
+  return (
+	<>
+	<Subnav />
+    <div className="col-lg-8 mx-auto p-4 py-md-5">
+      <h1>Login Page</h1>
+      <p>Check out our Login!</p>
+    </div>
+	</>
+  );
+};
+
+export default Login;
